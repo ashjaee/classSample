e5117e75c92cd109acb3b5fd4b7cffaeea6fc794
unknown <mm.ashjaei@gmail.com>
2021-09-13 11:27:33 +0430

    version2

--- classSampleMultiple.py
+++ classSampleMultiple.py
@@ -1,14 +1,14 @@
 class A:
-    def rpt_func(self):
+    def rpt_func(self,no1):
         print("class A")
 
 class B:
-    def rpt_func(self):
+    def rpt_func(self,no1,no2):
         print("class B")
 
 class C(A,B):
-    def rpt_func(self):
+    def rpt_func(self,no1,no2,no3):
         print("class C")
 
 c = C()
-print(c.rpt_func())
+c.rpt_func(20)
